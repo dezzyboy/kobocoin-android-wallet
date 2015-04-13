584c12ecb8ff2a9d1cdbb27f2a5b9a4cfb935643
Leandro Machado <sr.machado@gmail.com>
2015-04-13 10:06:28 -0300

    Kobocoin Android Wallet
    Working but I am not downloading the blockchain properly, stuck in 12 or
    13 hours behind.

---
+++ wallet/target/generated-sources/r/com/bushstar/kobocoin_android_wallet/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.bushstar.kobocoin_android_wallet;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
